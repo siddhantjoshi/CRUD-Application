76b8e19a2710a7af1af1a6ed171e19a5d98f317d
Siddhant joshi <46080976+siddhantjoshi@users.noreply.github.com>
2021-05-17 07:50:05 +0530

    Delete CRUD Using MySQLi directory

--- CRUD Using MySQLi/asset/header.php
+++
@@ -1,8 +0,0 @@
-<!--nav bar-->
-<div class="navbar navbar-dark bg-dark">
-	<div class="container">
-		<a class="navbar-brand" href="#">
-			CRUD Applcation 
-		</a>
-	</div>
-</div>
--- CRUD Using MySQLi/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("inc/function.php");
-
-if(isset($_GET['user_id'])){
-	$user_id=$_GET['user_id'];
-	deleteUser($user_id);
-}
-?>
